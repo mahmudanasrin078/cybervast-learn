fc15bf32822b1468cd3c5b7ea699933c7e970eaa
mahmudanasrin078 <mahmudanasrin078@gmail.com>
2026-07-11 11:51:39 +0600

    initialize project and installed dependencies

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-black flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-violet-500">
+        CyberVast Learn
+      </h1>
+    </div>
+  );
+}
+
+export default App;
